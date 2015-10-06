d00109f02f15d24e1e2cb85f94a46849333cfceb
Michael <mike.bdg@gmail.com>
2015-10-06 13:52:15 +0800

    Include jar

---
+++ src/cs3213/jms/order/JmsPipe.java
@@ -0,0 +1,17 @@
+package cs3213.jms.order;
+
+/**
+ * Matric 1:
+ * Name   1:
+ * 
+ * Matric 2:
+ * Name   2:
+ *
+ * This file implements a pipe that transfer messages using JMS.
+ */
+
+public class JmsPipe implements IPipe {
+    
+    // your code here
+    
+}
